0388ae217c2fe052cc472e9cb026f08cf2394ff2
kev560 <singhkevin132@gmail.com>
2015-04-24 16:36:34 -0700

    change loop

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -5,25 +5,29 @@ using namespace std;
 int main()
 {
 	string k;
-	cin >> k;
-	int x = k.size();
-	int count = 0;
-	for (int i = 0; i < x; i++)
-	{
-		for (int j = x-1; j >= 0; j--)
+	do{
+		cout << "\n Enter word:" << endl;
+		cin >> k;
+		int x = k.size();
+		int count = 0;
+		for (int i = 0; i < x; i++)
 		{
-			if (k[i] == k[j])
+			for (int j = x - 1; j >= 0; j--)
 			{
-				count += 1;
-			}
+				if (k[i] == k[j])
+				{
+					count += 1;
+				}
 
+			}
 		}
+		if (count == x)
+
+			cout << k << " is not a palindrome" << endl;
+		else
+			cout << k << " is a palindrome" << endl;
 	}
-	if (count == x)
-	
-		cout << k << " is not a palindrome" << endl;
-	else
-		cout << k << " is a palindrome" << endl;
+	while (k != "EXIT");
 	
 	system("pause");
 	return 0;
